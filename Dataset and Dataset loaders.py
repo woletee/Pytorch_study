6404cc6216b6913ba484613fb67398c0ca06392a
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-25 22:58:03 +0900

    Update Dataset and Dataset loaders.py

--- Dataset and Dataset loaders.py
+++ Dataset and Dataset loaders.py
@@ -5,3 +5,6 @@ import numpy as np
 import math
 class WineDataset(Dataset):
     def __init__(self):
+        # Data loading
+        xy = np.loadtxt('./wine.csv', delimiter=",", dtype=np.float32, skiprows=1)
+        self.x = torch.from_numpy(xy[:, 1:])
