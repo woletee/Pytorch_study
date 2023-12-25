134fa970316bf2792a96a4812368fc44d80dcba3
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-25 23:38:19 +0900

    Update Dataset transforms.py

--- Dataset transforms.py
+++ Dataset transforms.py
@@ -3,3 +3,11 @@ import torchvision
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import math
+class WineDataset(Dataset):
+    def __init__(self, transform=None):
+        # Data loading
+        xy = np.loadtxt('./wine.csv', delimiter=",", dtype=np.float32, skiprows=1)
+        self.x = torch.from_numpy(xy[:, 1:])
+        self.y = torch.from_numpy(xy[:, [0]])
+        self.n_samples = xy.shape[0]
+        self.transform = transform
