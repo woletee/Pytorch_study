f041d5cbfdd81ddd9c5574e90e7902ce506b0c0c
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-26 12:20:17 +0900

    Update Digit_classification.py

--- Digit_classification.py
+++ Digit_classification.py
@@ -17,3 +17,5 @@ learning_rate = 0.001
 # MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
