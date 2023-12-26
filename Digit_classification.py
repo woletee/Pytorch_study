b1c467ea01c70fe86a971dc14e68c64cd62fd8d9
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-26 12:19:36 +0900

    Update Digit_classification.py

--- Digit_classification.py
+++ Digit_classification.py
@@ -4,3 +4,5 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
