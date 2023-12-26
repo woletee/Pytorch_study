f95653ecee0747b18e49bb992dce7e4c74464319
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-26 10:39:32 +0900

    Update softmax and cross entropy.py

--- softmax and cross entropy.py
+++ softmax and cross entropy.py
@@ -11,3 +11,7 @@ x_np = np.array([2.0, 1.0, 0.1])
 # Applying the softmax function with NumPy
 outputs_np = softmax(x_np)
 print('Softmax using NumPy:', outputs_np)
+# Softmax using PyTorch
+x_torch = torch.tensor([2.0, 1.0, 0.1])
+outputs_torch = torch.softmax(x_torch, dim=0)
+print('Softmax using PyTorch:', outputs_torch)
