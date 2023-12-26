d42c660fcfb932d50f449da51fc3ad0445ad2550
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-26 12:19:56 +0900

    Update Digit_classification.py

--- Digit_classification.py
+++ Digit_classification.py
@@ -11,3 +11,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size = 784  # 28x28
 hidden_size = 100
 num_classes = 10  # MNIST has 10 classes (digits 0-9)
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
