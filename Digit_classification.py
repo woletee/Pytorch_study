b38bfb0397e3003d2ba213f5817cab8b9352d294
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-26 12:19:47 +0900

    Update Digit_classification.py

--- Digit_classification.py
+++ Digit_classification.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyperparameters
+input_size = 784  # 28x28
+hidden_size = 100
+num_classes = 10  # MNIST has 10 classes (digits 0-9)
