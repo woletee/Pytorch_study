94f32bdb447ea5a954db6e312f3424c407b277ba
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-27 15:21:54 +0900

    Update Backpropagation.py

--- Backpropagation.py
+++ Backpropagation.py
@@ -7,3 +7,14 @@ import torch.optim as optim
 X = torch.tensor([[1.0], [2.0], [3.0], [4.0]])
 Y = torch.tensor([[2.0], [4.0], [6.0], [8.0]])
 
+# 2. Model definition
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc1 = nn.Linear(1, 5)  # One input feature, 5 hidden units
+        self.fc2 = nn.Linear(5, 1)  # 5 hidden units, one output
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
