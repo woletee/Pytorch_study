7a4dbbdccf4c362779bf432e92e750fa90a42a7d
Mary Liyew <118590747+woletee@users.noreply.github.com>
2023-12-27 15:21:43 +0900

    Update Backpropagation.py

--- Backpropagation.py
+++ Backpropagation.py
@@ -1,1 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# 1. Data preparation
+# Simple dataset (X: input, Y: target)
+X = torch.tensor([[1.0], [2.0], [3.0], [4.0]])
+Y = torch.tensor([[2.0], [4.0], [6.0], [8.0]])
 
