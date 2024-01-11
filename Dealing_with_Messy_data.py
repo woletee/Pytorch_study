d91281aaad380b366e174f0e21fa586424b1e03a
Mary Liyew <118590747+woletee@users.noreply.github.com>
2024-01-11 16:19:05 +0900

    Update Dealing_with_Messy_data.py

--- Dealing_with_Messy_data.py
+++ Dealing_with_Messy_data.py
@@ -1,3 +1,6 @@
 import pandas as pd
 data=pd.read_csv("energydata_complete.csv")
 data=data.drop(columns=["date"])
+cols=data.columns
+num_cols=data._get_numeric_data().columns
+list(set(cols)-set(num_cols))
