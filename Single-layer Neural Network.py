cee747fbd7288ae13f9ce85e61da6d7e23eb93d6
Mary Liyew <118590747+woletee@users.noreply.github.com>
2024-01-11 12:26:25 +0900

    Update Single-layer Neural Network.py

--- Single-layer Neural Network.py
+++ Single-layer Neural Network.py
@@ -7,3 +7,9 @@ y= torch.randint(0, 2, (100,1)).type(torch.FloatTensor)
 #next we have to define the requirements for the model and then we will store it in a variable named model
 #basically the above coe we have writtten is for  single layer but if 
 #we used two activation functions then it would be for muliptle layers
+model=nn.Sequential(
+      nn.Linear(5,1),
+      nn.ReLU())
+loss_function = torch.nn.MSELoss()
+#this line of the code has defined the optimiaer fo ourt model
+optimizer=torch.optim.Adam(model.parameters(),lr=0.01)
